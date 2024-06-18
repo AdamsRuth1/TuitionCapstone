37cddd819b3b40358f91c4a1665ac3c3a024af6d
Adams ruth <adamsruth4080@gmail.com>
2024-06-18 06:28:45 -0700

    ressolving deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ export default {
         customLightGray: "#606569",
         customLine: "#CCCCCC",
         customEnroll: "#606569",
+        customrAdd: "#F8FAFE",
       },
      
       fontFamily: {
